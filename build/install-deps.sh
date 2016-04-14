7160c93fdcad0028d8979561eccd41b24f266264
Michal Tesar <michal@revolutiongeneration.co.uk>
2016-04-14 10:06:05 +0100

    link /usr/lib/erlang to /usr/local/lib/erlang

--- build/install-deps.sh
+++ build/install-deps.sh
@@ -5,4 +5,5 @@ wget http://erlang.org/download/otp_src_R15B03-1.tar.gz -P /usr/src/
 tar -C /opt/ -zxf /usr/src/otp_src_R15B03-1.tar.gz
 cd /opt/otp_src_R15B03/
 ./configure && make && make install
+ln -s /usr/local/lib/erlang/ /usr/lib/erlang
 
