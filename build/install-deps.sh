86b63a4ba29f0ef1a6e4e35ec1a476b54494739f
Michal Tesar <michal@revolutiongeneration.co.uk>
2016-04-26 12:59:22 +0100

    automatic installe

--- build/install-deps.sh
+++ build/install-deps.sh
@@ -5,5 +5,5 @@ cd /tmp
 wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
 dpkg -i erlang-solutions_1.0_all.deb
 apt-get update
-apt-get install esl-erlang
+apt-get install -y esl-erlang
 
